6c092a881b1a6b23f9c97175add7640521b5cc17
Ellecn <94251631+Ellecn@users.noreply.github.com>
2022-06-10 23:18:46 +0200

    Add project with commonly used classes

---
+++ Common/WikipediaPage.cs
@@ -0,0 +1,31 @@
+﻿namespace Common
+{
+    public class WikipediaPage
+    {
+        public WikipediaPage(long pageId, string title, Position position)
+        {
+            PageId = pageId;
+            Title = title;
+            Summary = "";
+            Position = position;
+        }
+
+        public long PageId { get; }
+
+        public string Title { get; }
+
+        public string Summary { get; set; }
+
+        public Position Position { get; }
+
+        public double Distance { get; set; }
+
+        public string URL
+        {
+            get
+            {
+                return string.Format("https://de.wikipedia.org/wiki/{0}", Title.Replace(' ', '_'));
+            }
+        }
+    }
+}
