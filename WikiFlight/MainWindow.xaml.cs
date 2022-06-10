12a7794f26882c4006166230ea06e5fc01a09d59
Ellecn <94251631+Ellecn@users.noreply.github.com>
2022-06-10 18:41:07 +0200

    Create dummy GUI

--- WikiFlight/MainWindow.xaml.cs
+++ WikiFlight/MainWindow.xaml.cs
@@ -23,6 +23,18 @@ namespace WikiFlight
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lstPages.Items.Add(new DummyPage());
+            }
+        }
+
+        class DummyPage
+        {
+            public string Title { get { return "Title"; } }
+            public string Content { get { return "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."; } }
+
         }
     }
 }
